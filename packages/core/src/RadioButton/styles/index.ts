0c4e52aac51639f8b667487530f18a8355ac99e2
Maxim Zadorkin <mazadorkin@sberbank.ru>
2022-07-28 15:26:16 +0300

    feat(RadioButton): Обновление стилей

--- packages/core/src/RadioButton/styles/index.ts
+++ packages/core/src/RadioButton/styles/index.ts
@@ -1,134 +1,161 @@
-import { KeysFromUseStyles, makeStyles, typography } from '@core';
+import { KeysFromUseStyles, makeStyles, PALETTE_COLORS, typography } from '@core';
 import { paramsToCss } from '@core/utils/paramsToCss';
+import { CSSObject } from '@emotion/react';
 import { RadioButtonStyleParams } from './types';
 
 export const useStyles = makeStyles((
     { palette, transitions },
-    { size, hover, disableFocus, hasError, disabled, checked }: RadioButtonStyleParams,
-) => ({
-    root: [
-        {
-            position: 'relative',
-            display: 'flex',
-            alignItems: 'center',
-            backgroundColor: 'transparent',
-            cursor: 'pointer',
+    {
+        color = PALETTE_COLORS.brand,
+        size,
+        hover,
+        disableFocus,
+        disabled,
+        checked,
+    }: RadioButtonStyleParams,
+) => {
+    const baseColorProps: CSSObject = {
+        border: '1px solid',
+        borderColor: palette.border.secondary,
+        ...checked && {
+            backgroundColor: palette.colors[color].default,
+            borderColor: palette.colors[color].default,
         },
-        disabled && {
-            cursor: 'not-allowed',
+        ...checked && hover && {
+            borderColor: palette.colors[color].hover,
+            backgroundColor: palette.colors[color].hover,
         },
-    ],
+    };
 
-    input: [
-        {
-            position: 'absolute',
-            appearance: 'none',
-        },
-        !disableFocus && {
-            '&:focus-visible + .SxRadioButton-marker': {
-                borderWidth: 2,
-                borderColor: palette.border.focus.dark,
-            },
-        },
-        !checked && !hasError && !disableFocus && !disabled && {
-            '&:focus-visible + .SxRadioButton-marker': {
+    return ({
+        root: [
+            {
+                position: 'relative',
+                display: 'flex',
+                alignItems: 'center',
                 backgroundColor: 'transparent',
+                cursor: 'pointer',
             },
-        },
-        checked && !hasError && !disableFocus && {
-            '&:focus-visible + .SxRadioButton-marker': {
-                backgroundColor: palette.colors.brand.default,
+            disabled && {
+                cursor: 'not-allowed',
             },
-        },
-        checked && hasError && !disableFocus && {
-            '&:focus-visible + .SxRadioButton-marker': {
-                backgroundColor: palette.colors.danger.default,
-            },
-        },
-    ],
+        ],
 
-    marker: [
-        {
-            borderRadius: '100%',
-            boxSizing: 'border-box',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            // backgroundColor: palette.RadioButton.enabled.bg,
-            border: `1px solid ${palette.border.secondary}`,
-            transition: transitions.create(['background-color', 'border-color']),
-        },
-        paramsToCss(size)({
-            small: {
-                height: 16,
-                width: 16,
+        input: [
+            {
+                position: 'absolute',
+                appearance: 'none',
             },
-            medium: {
-                height: 18,
-                width: 18,
+            !disableFocus && {
+                '&:focus-visible + .SxRadioButton-marker': {
+                    borderWidth: 3,
+                    borderColor: palette.border.focus.dark,
+                    outline: 'none',
+                },
             },
-            large: {
-                height: 20,
-                width: 20,
+            !checked && !disableFocus && !disabled && {
+                '&:focus-visible + .SxRadioButton-marker': {
+                    backgroundColor: 'transparent',
+                    outline: 'none',
+                },
             },
-        }),
+        ],
 
-        checked && {
-            backgroundColor: palette.colors.brand.default,
-            borderColor: 'transparent',
-        },
-        hover && checked && {
-            backgroundColor: palette.colors.brand.hover,
-        },
-        hasError && {
-            borderColor: palette.colors.danger.default,
-        },
-        hasError && checked && {
-            backgroundColor: palette.colors.danger.default,
-        },
-        (disabled || (disabled && hasError)) && {
-            backgroundColor: palette.text.tertiary,
-            borderColor: 'transparent',
-        },
-        disabled && checked && {
-            backgroundColor: palette.text.tertiary,
-        },
-    ],
-    markerDot: [
-        {
-            position: 'absolute',
-            borderRadius: '100%',
-            width: 8,
-            height: 8,
-            backgroundColor: 'transparent',
-            transition: transitions.create('background-color'),
-        },
-        size === 'small' && {
-            width: 6,
-            height: 6,
-        },
-        disabled && {
-            backgroundColor: 'transparent',
-        },
-        (checked || (checked && !hasError) || (checked && disabled))
-            && {
-                backgroundColor: palette.text.constant,
+        marker: [
+            {
+                borderRadius: '100%',
+                boxSizing: 'border-box',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                transition: transitions.create(['background-color', 'border-color']),
             },
-        !checked && !disabled && hover && {
-            backgroundColor: palette.border.secondary,
-        },
-    ],
-    content: [
-        {
-            marginLeft: 12,
-        },
-        paramsToCss(size)({
-            small: typography.Text.M.Regular,
-            medium: typography.Text.L.Regular,
-            large: typography.Text.XL.Regular,
-        }),
-    ],
-}), { name: 'SxRadioButton' });
+
+            paramsToCss(size)({
+                small: {
+                    height: 16,
+                    width: 16,
+                },
+                medium: {
+                    height: 18,
+                    width: 18,
+                },
+                large: {
+                    height: 20,
+                    width: 20,
+                },
+            }),
+
+            paramsToCss(color)({
+                [color]: {
+                    border: '2px solid',
+                    borderColor: palette.colors[color].default,
+                    ...checked && {
+                        backgroundColor: palette.colors[color].default,
+                        borderColor: 'transparent',
+                    },
+                    ...checked && hover && {
+                        backgroundColor: palette.colors[color].hover,
+                    },
+                },
+                [PALETTE_COLORS.brand]: baseColorProps,
+                [PALETTE_COLORS.secondary]: baseColorProps,
+            }),
+
+            disabled && {
+                border: '1px solid',
+                borderColor: palette.disabled.border,
+            },
+            disabled && checked && {
+                borderColor: 'transparent',
+                backgroundColor: palette.disabled.bg,
+            },
+        ],
+
+        markerDot: [
+            {
+                position: 'absolute',
+                borderRadius: '100%',
+                backgroundColor: 'transparent',
+                transition: transitions.create('background-color'),
+            },
+
+            paramsToCss(size)({
+                [size]: {
+                    width: 8,
+                    height: 8,
+                },
+                small: {
+                    width: 6,
+                    height: 6,
+                },
+            }),
+
+            paramsToCss(color)({
+                [color]: {
+                    backgroundColor: palette.colors[color].contrastText,
+                    ...hover && !checked && {
+                        backgroundColor: palette.text.tertiary,
+                    },
+                    ...disabled && {
+                        backgroundColor: palette.colors[color].contrastText,
+                    },
+                },
+            }),
+        ],
+
+        content: [
+            {
+                marginLeft: 12,
+            },
+            paramsToCss(size)({
+                small: typography.Text.M.Regular,
+                medium: typography.Text.L.Regular,
+                large: typography.Text.XL.Regular,
+            }),
+        ],
+    });
+}, { name: 'SxRadioButton' });
 
 export type RadioButtonStyleKeys = KeysFromUseStyles<typeof useStyles>
 
