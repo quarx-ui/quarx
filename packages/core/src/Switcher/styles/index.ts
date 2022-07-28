178ddae83a2a75de545d7ad5ece8da497aba6ac8
Maxim Zadorkin <mazadorkin@sberbank.ru>
2022-07-28 15:27:35 +0300

    feat(Switcher): Обновление стилей

--- packages/core/src/Switcher/styles/index.ts
+++ packages/core/src/Switcher/styles/index.ts
@@ -1,164 +1,228 @@
-import { colors, SWITCHER_POSITION, typography, KeysFromUseStyles, makeStyles } from '@core';
+import { SWITCHER_POSITION, typography, KeysFromUseStyles, makeStyles, PALETTE_COLORS } from '@core';
 import { paramsToCss } from '@core/utils/paramsToCss';
 import { SwitcherStyleParams } from './types';
-// TODO: colors from palette
 
 export const useStyles = makeStyles((
     { palette, transitions },
-    { size, checked, hasError, disabled, hover, position, disableFocus }: SwitcherStyleParams,
-) => ({
-    wrapper: { display: 'inline-flex' },
+    {
+        size,
+        checked,
+        color = PALETTE_COLORS.brand,
+        disabled,
+        hover,
+        position,
+        disableFocus,
+    }: SwitcherStyleParams,
+) => {
+    const toggleWrapperStates = {
+        ...checked && {
+            border: 'none',
+            backgroundColor: palette.colors[color].default,
+        },
+        ...checked && hover && {
+            border: 'none',
+            backgroundColor: palette.colors[color].hover,
+        },
+    };
 
-    root: [
-        {
-            display: 'flex',
-            alignItems: 'center',
-            boxSizing: 'border-box',
-            cursor: 'pointer',
+    const toggleStates = {
+        ...hover && {
+            borderRadius: 8,
         },
-        disabled && {
-            cursor: 'not-allowed',
+        ...checked && {
+            backgroundColor: palette.colors[color].contrastText,
         },
-    ],
+    };
 
-    input: [
-        {
-            margin: 0,
-            appearance: 'none',
-        },
-        !disableFocus && {
-            '&:focus-visible + .SxSwitcher-toggleContainer': {
-                borderColor: palette.border.focus.dark,
-                boxShadow: `inset 0 0 0 1px ${palette.border.focus.dark}`,
-                outline: 'none',
+    return {
+        wrapper: { display: 'inline-flex' },
+
+        root: [
+            {
+                display: 'flex',
+                alignItems: 'center',
+                boxSizing: 'border-box',
+                cursor: 'pointer',
             },
-        },
-    ],
+            disabled && {
+                cursor: 'not-allowed',
+            },
+        ],
 
-    toggleContainer: [
-        {
-            display: 'flex',
-            alignItems: 'center',
-            boxSizing: 'border-box',
-            margin: 0,
-            padding: 0,
-            borderRadius: 10,
-            transition: transitions.create(['color', 'background-color', 'border-color', 'box-shadow'], {
-                duration: transitions.duration.shortest,
+        input: [
+            {
+                margin: 0,
+                appearance: 'none',
+            },
+            !disableFocus && {
+                '&:focus-visible + .SxSwitcher-toggleContainer': {
+                    borderColor: palette.border.focus.dark,
+                    outline: 'none',
+                    ...paramsToCss(color)({
+                        [PALETTE_COLORS.brand]: {
+                            boxShadow: `inset 0px 0px 0px 1px ${palette.border.focus.dark}`,
+                        },
+                        [PALETTE_COLORS.secondary]: {
+                            boxShadow: `inset 0px 0px 0px 1px ${palette.border.focus.dark}`,
+                        },
+                    }),
+                    ...checked && {
+                        boxShadow: `inset 0px 0px 0px 2px ${palette.border.focus.dark}`,
+                    },
+                },
+            },
+        ],
+
+        toggleContainer: [
+            {
+                display: 'flex',
+                position: 'relative',
+                alignItems: 'center',
+                boxSizing: 'border-box',
+                margin: 0,
+                padding: 0,
+                borderRadius: 10,
+                transition: transitions.create(['color', 'background-color', 'border-color'], {
+                    duration: transitions.duration.shortest,
+                }),
+            },
+
+            paramsToCss(color)({
+                [color]: {
+                    border: '2px solid',
+                    borderColor: palette.colors[color].default,
+                    backgroundColor: 'transparent',
+                    ...hover && {
+                        borderColor: palette.colors[color].hover,
+                    },
+                    ...toggleWrapperStates,
+                },
+                [PALETTE_COLORS.brand]: {
+                    border: '1px solid',
+                    borderColor: palette.border.secondary,
+                    backgroundColor: 'transparent',
+                    ...hover && {
+                        borderColor: palette.text.tertiary,
+                    },
+                    ...toggleWrapperStates,
+                },
+                [PALETTE_COLORS.secondary]: {
+                    border: '1px solid',
+                    borderColor: palette.border.secondary,
+                    backgroundColor: 'transparent',
+                    ...hover && {
+                        borderColor: palette.text.tertiary,
+                    },
+                    ...toggleWrapperStates,
+                },
             }),
-            border: '1px solid',
-            borderColor: colors.Black[24],
-            backgroundColor: palette.background.main,
-        },
-        hover && {
-            borderColor: colors.Black[32],
-        },
-        checked && {
-            border: '1px solid',
-            borderColor: colors.Emerald[500],
-            backgroundColor: colors.Emerald[500],
-        },
-        checked && hover && {
-            border: '1px solid',
-            borderColor: colors.Emerald[400],
-            backgroundColor: colors.Emerald[400],
-        },
-        hasError && {
-            borderColor: colors.Red[600],
-            boxShadow: `inset 0 0 0 1px ${colors.Red[600]}`,
-        },
-        hasError && checked && {
-            backgroundColor: colors.Red[600],
-            borderColor: colors.Red[600],
-        },
-        disabled && {
-            borderColor: '#E1E4E5',
-            boxShadow: 'none',
-            backgroundColor: colors.White[100],
-        },
-        disabled && checked && {
-            border: '1px solid',
-            borderColor: '#E1E4E5',
-            backgroundColor: '#E1E4E5',
-        },
-        paramsToCss(size)({
-            small: {
-                minWidth: 26,
-                width: 26,
-                height: 16,
-            },
-            medium: {
-                width: 30,
-                minWidth: 30,
-                height: 18,
-            },
-            large: {
-                minWidth: 34,
-                width: 34,
-                height: 20,
-            },
-        }),
-    ],
 
-    toggle: [
-        {
-            marginRight: 2,
-            marginLeft: 2,
-            borderRadius: 10,
-            backgroundColor: colors.Black[24],
-            transition: transitions.create(['background-color', 'margin'], {
-                duration: transitions.duration.shortest,
+            disabled && {
+                border: '1px solid',
+                borderColor: palette.disabled.border,
+                backgroundColor: 'transparent',
+            },
+            disabled && checked && {
+                border: 'none',
+                backgroundColor: palette.disabled.bg,
+            },
+
+            paramsToCss(size)({
+                small: {
+                    minWidth: 26,
+                    width: 26,
+                    height: 16,
+                },
+                medium: {
+                    width: 30,
+                    minWidth: 30,
+                    height: 18,
+                },
+                large: {
+                    minWidth: 34,
+                    width: 34,
+                    height: 20,
+                },
             }),
-        },
-        hover && {
-            backgroundColor: colors.Black[32],
-        },
-        checked && {
-            backgroundColor: colors.White[100],
-        },
-        disabled && {
-            backgroundColor: colors.Black[8],
-        },
-        disabled && checked && {
-            backgroundColor: colors.White[100],
-        },
-        paramsToCss(size)({
-            small: {
-                width: 10,
-                minWidth: 10,
-                height: 10,
-            },
-            medium: {
-                width: 12,
-                minWidth: 12,
-                height: 12,
-            },
-            large: {
-                width: 14,
-                minWidth: 14,
-                height: 14,
-            },
-        }),
-        checked && paramsToCss(size)({
-            small: { marginLeft: 11 },
-            medium: { marginLeft: 13 },
-            large: { marginLeft: 15 },
-        }),
-    ],
+        ],
 
-    content: [
-        { color: palette.text.main },
-        paramsToCss(size)({
-            small: typography.Text.M.Regular,
-            medium: typography.Text.L.Regular,
-            large: typography.Text.XL.Regular,
-        }),
-        paramsToCss(position)({
-            [SWITCHER_POSITION.left]: { marginLeft: 12 },
-            [SWITCHER_POSITION.right]: { marginRight: 12 },
-        }),
-    ],
-}), { name: 'SxSwitcher' });
+        toggle: [
+            {
+                position: 'absolute',
+                borderRadius: 10,
+                transition: transitions.create(['background-color', 'border-color', 'left'], {
+                    duration: transitions.duration.shortest,
+                }),
+            },
+
+            paramsToCss(size)({
+                small: {
+                    width: 10,
+                    minWidth: 10,
+                    height: 10,
+                },
+                medium: {
+                    width: 12,
+                    minWidth: 12,
+                    height: 12,
+                },
+                large: {
+                    width: 14,
+                    minWidth: 14,
+                    height: 14,
+                },
+            }),
+            paramsToCss(size)({
+                small: { left: 2 },
+                medium: { left: 2 },
+                large: { left: 2 },
+            }),
+            checked && paramsToCss(size)({
+                small: { left: 13 },
+                medium: { left: 15 },
+                large: { left: 16 },
+            }),
+
+            paramsToCss(color)({
+                [color]: {
+                    backgroundColor: palette.colors[color].border,
+                    borderRadius: 10,
+                    ...toggleStates,
+                },
+                [PALETTE_COLORS.brand]: {
+                    backgroundColor: palette.text.tertiary,
+                    borderRadius: 10,
+                    ...toggleStates,
+                },
+                [PALETTE_COLORS.secondary]: {
+                    backgroundColor: palette.text.tertiary,
+                    borderRadius: 10,
+                    ...toggleStates,
+                },
+            }),
+
+            disabled && {
+                backgroundColor: palette.disabled.border,
+            },
+            disabled && checked && {
+                backgroundColor: palette.text.constant,
+            },
+        ],
+
+        content: [
+            { color: palette.text.main },
+            paramsToCss(size)({
+                small: typography.Text.M.Regular,
+                medium: typography.Text.L.Regular,
+                large: typography.Text.XL.Regular,
+            }),
+            paramsToCss(position)({
+                [SWITCHER_POSITION.left]: { marginLeft: 12 },
+                [SWITCHER_POSITION.right]: { marginRight: 12 },
+            }),
+        ],
+    };
+}, { name: 'SxSwitcher' });
 
 export type SwitcherStyleKeys = KeysFromUseStyles<typeof useStyles>;
 export * from './types';
