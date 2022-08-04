51c26a7b0669b02ce42f4b97e9165ece500618a8
Iskander Aydynov <iaydynov@sberbank.ru>
2022-08-04 14:32:23 +0300

    fix(makeStyles): Исправлена типизация makeStyles, удалены лишние проверки на подфункции

--- packages/core/styles/engine/types.ts
+++ packages/core/styles/engine/types.ts
@@ -5,7 +5,7 @@ import { SerializedStyles } from '@emotion/react';
 export type StylesMap<ClassKey extends string = string> = Record<ClassKey, SerializedStyles>;
 
 export type Classes<Keys extends string> = Partial<Record<Keys, string>>
-export type Styles<ClassKey extends string = string, > = Record<ClassKey, CSSInterpolation>;
+export type Styles<ClassKey extends string = string> = Record<ClassKey, CSSInterpolation>;
 
 export type StylesWithCallback<
     ClassKey extends string = string,
@@ -15,7 +15,7 @@ export type StylesWithCallback<
     CSSInterpolation | ((theme: Theme, props: Required<Props>) => CSSInterpolation)
 >;
 
-export type StylesCallback<Keys extends string, Props extends object, Theme> = (theme: Theme, props: Required<Props>) => Partial<Styles<Keys>>
+export type StylesCallback<Keys extends string, Props extends object, Theme> = (theme: Theme, props: Required<Props>) => Styles<Keys>
 
 export interface WithClassesAndStyles<
     Keys extends string,
