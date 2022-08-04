fcbe5a0f6cc989489c9a9b05eccceca7c4f12daa
Iskander Aydynov <iaydynov@sberbank.ru>
2022-08-04 14:42:17 +0300

    docs(elevations): Добавлено описание

--- packages/core/storybook/docsPage.tsx
+++ packages/core/storybook/docsPage.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Title, Primary, Subtitle, Stories, ArgsTable, PRIMARY_STORY } from '@storybook/addon-docs';
 import { typography } from '@core';
 
-export function withDocsPage(Documentation: React.ComponentType) {
-    return () => (
-        <>
-            <Title />
-            <Primary />
-            <Subtitle>Описание пропсов</Subtitle>
-            <ArgsTable story={PRIMARY_STORY} />
-            <div style={{ ...typography.Text.M.Regular }}><Documentation /></div>
-            <Stories title="Примеры использования" />
-        </>
-    );
-}
+export const withDocsPage = (Documentation: FC) => () => (
+    <>
+        <Title />
+        <Primary />
+        <Subtitle>Описание пропсов</Subtitle>
+        <ArgsTable story={PRIMARY_STORY} />
+        <div style={typography.Text.M.Regular}><Documentation /></div>
+        <Stories title="Примеры использования" />
+    </>
+);
