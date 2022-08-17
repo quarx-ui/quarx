6867af29d0459edf375a0b03952c4bf5f5779fd9
lear <mgk.lear@gmail.com>
2022-08-17 15:10:18 +0300

    refactor: Удалены лишние зависимости и неиспользуемый код

--- packages/core/styles/engine/styles/index.ts
+++
@@ -1,5 +0,0 @@
-export { makeStylesKit } from './makeStylesKit';
-export { withStylesKit } from './withStylesKit';
-export { styledKit } from './styledKit';
-export { StylesProvider } from './StylesProvider';
-export { createGenerateClassName } from './createGenerateClassName';
