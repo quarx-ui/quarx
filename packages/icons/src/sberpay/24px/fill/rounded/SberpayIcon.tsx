b3d8d7f43bb03b84c6da1306935798e1c40fdf1e
aak-lear <aak-lear@users.noreply.github.com>
2023-01-12 07:12:25 +0000

    figma: Автоматическая выгрузка иконок

--- packages/icons/src/sberpay/24px/fill/rounded/SberpayIcon.tsx
+++ packages/icons/src/sberpay/24px/fill/rounded/SberpayIcon.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import React, { forwardRef } from 'react';
 
 export const SberpayIcon = forwardRef<
 SVGSVGElement,
--- packages/icons/src/share/24px/stroke/rounded/ShareIcon.tsx
+++ packages/icons/src/share/24px/stroke/rounded/ShareIcon.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import React, { forwardRef } from 'react';
 
 export const ShareIcon = forwardRef<
 SVGSVGElement,
