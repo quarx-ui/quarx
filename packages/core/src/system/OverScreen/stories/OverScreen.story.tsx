3e758cb894d591b84cfcdeafc2929ab44a50b48e
Iskander Aydynov <iaydynov@sberbank.ru>
2023-02-13 15:33:11 +0300

    docs(OverScreen): Добавлена документация

--- packages/core/src/system/OverScreen/stories/OverScreen.story.tsx
+++ packages/core/src/system/OverScreen/stories/OverScreen.story.tsx
@@ -1,8 +1,9 @@
-import { OVER_SCREEN_APPEARANCE, OVER_SCREEN_ORIGIN, OVER_SCREEN_PLACEMENT /* OverScreen */ } from '@core';
-// import { defineCategory, excludeProp } from '@core/storybook/templateParams';
-// import { BASE_ARG_TYPES } from '@core/storybook/BASE_ARG_TYPES';
+import { OVER_SCREEN_APPEARANCE, OVER_SCREEN_ORIGIN, OVER_SCREEN_PLACEMENT, OverScreen } from '@core';
+import { defineCategory, excludeProp } from '@core/storybook/templateParams';
+import { BASE_ARG_TYPES } from '@core/storybook/BASE_ARG_TYPES';
+import { STORY_PATHS } from '@quarx-ui/storybook/utils';
 import { OverScreenProps } from '..';
-import { Origins, Placements, Appearances, Offset, Margin, BooleanVariants /* Sandbox */ } from './common';
+import { Origins, Placements, Appearances, Offset, Margin, BooleanVariants, Sandbox } from './common';
 
 const defaultArgs: Partial<OverScreenProps> = {
     origin: OVER_SCREEN_ORIGIN.right,
@@ -16,61 +17,48 @@ const defaultArgs: Partial<OverScreenProps> = {
     disableCloseByEscape: false,
 };
 
-// export default {
-//     title: 'core/OverScreen',
-//     component: OverScreen,
-//     argTypes: {
-//         ...defineCategory('Расположение', {
-//             origin: {},
-//             placement: {},
-//             offset: {},
-//             margin: {},
-//         }),
-//         ...defineCategory('Поведение', {
-//             open: {},
-//             appearance: {},
-//             keepMounted: {},
-//             disableCloseByClickAway: {},
-//             disableCloseByEscape: {},
-//             disableScrollLock: {},
-//             disablePortal: {},
-//             disableBackdrop: {},
-//             onClose: {},
-//         }),
-//         ...defineCategory('Компоненты', {
-//             children: { control: false },
-//             TransitionProps: {},
-//             DelayedMounterProps: {},
-//             BackdropProps: {},
-//             PortalProps: {},
-//         }),
-//         ...excludeProp(['permissions', 'hidden'], BASE_ARG_TYPES),
-//     },
-//     parameters: {
-//         actions: { disable: true },
-//     },
-//     args: defaultArgs,
-// };
+export default {
+    title: STORY_PATHS.core.components.system('OverScreen'),
+    component: OverScreen,
+    argTypes: {
+        ...defineCategory('Расположение', {
+            origin: {},
+            placement: {},
+            offset: {},
+            margin: {},
+        }),
+        ...defineCategory('Поведение', {
+            open: {},
+            appearance: {},
+            keepMounted: {},
+            disableCloseByClickAway: {},
+            disableCloseByEscape: {},
+            disableScrollLock: {},
+            disablePortal: {},
+            disableBackdrop: {},
+            onClose: {},
+        }),
+        ...defineCategory('Компоненты', {
+            children: { control: false },
+            TransitionProps: {},
+            DelayedMounterProps: {},
+            BackdropProps: {},
+            PortalProps: {},
+        }),
+        ...excludeProp(['permissions', 'hidden'], BASE_ARG_TYPES),
+    },
+    parameters: {
+        actions: { disable: true },
+    },
+    args: defaultArgs,
+};
 
-// export {
-//     Sandbox,
-//     Origins,
-//     Placements,
-//     Appearances,
-//     Offset,
-//     Margin,
-//     BooleanVariants,
-// };
-
-const variantsArray = [
+export {
+    Sandbox,
     Origins,
     Placements,
     Appearances,
     Offset,
     Margin,
     BooleanVariants,
-];
-
-variantsArray.forEach((variant, index) => {
-    variantsArray[index].args = defaultArgs;
-});
+};
