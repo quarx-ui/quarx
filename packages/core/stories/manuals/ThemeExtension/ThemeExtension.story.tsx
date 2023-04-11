b2b6b7af098eff988f4ef352f4dfc6a747aca399
Iskander Aydynov <iaydynov@sberbank.ru>
2023-04-11 16:34:26 +0300

    docs: Описание расширения дефолтных интерфейсов

---
+++ packages/core/stories/manuals/ThemeExtension/ThemeExtension.story.tsx
@@ -0,0 +1,34 @@
+import { BASE_ARG_TYPES } from '@core/storybook/BASE_ARG_TYPES';
+import { Description, Title } from '@storybook/addon-docs';
+import { Story } from '@storybook/react/types-6-0';
+import { Div } from '@storybook/components';
+import { STORY_PATHS } from '@quarx-ui/storybook/utils';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import description from './description.md';
+
+export default {
+    title: STORY_PATHS.core.manuals(),
+    argTypes: BASE_ARG_TYPES,
+    parameters: {
+        viewMode: 'docs',
+        previewTabs: {
+            canvas: { hidden: true },
+        },
+        docs: {
+            page: (): JSX.Element => (
+                <Div>
+                    <Title>
+                        Расширение темы
+                    </Title>
+                    <Description
+                        markdown={description}
+                    />
+                </Div>
+            ),
+        },
+    },
+};
+
+export const ThemeExtension: Story = () => <div />;
+ThemeExtension.storyName = 'Расширение темы';
