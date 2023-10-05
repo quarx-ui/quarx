65d5545f59b640c6a65c2709ef6dab5e31d2332b
aak-lear <54769688+aak-lear@users.noreply.github.com>
2023-10-05 21:49:01 +0300

    fix(ThemeProvider): Исправлено пробрасывание темы для случаев, когда она не определена изначально

--- packages/core/src/system/ThemeProvider/index.ts
+++ packages/core/src/system/ThemeProvider/index.ts
@@ -1,1 +1,2 @@
 export * from './ThemeProvider';
+export type { ThemeProviderProps } from './types';
--- packages/core/styles/engine/theme/hooks/useTheme/helpers.ts
+++ packages/core/styles/engine/theme/hooks/useTheme/helpers.ts
@@ -3,7 +3,7 @@ import { defaultTheme, Theme } from '@core';
 export const themeIsQx = (theme: Theme) => {
     const defaultThemeKeys = Object.keys(defaultTheme);
     const contextThemeKeys = Object.keys(theme);
-    const defaultKeysNotExist = defaultThemeKeys
+    const defaultKeysIsExist = defaultThemeKeys
         .every((themeKey) => contextThemeKeys.includes(themeKey));
-    return defaultKeysNotExist && Boolean(theme.palette);
+    return defaultKeysIsExist && Boolean(theme.palette);
 };
